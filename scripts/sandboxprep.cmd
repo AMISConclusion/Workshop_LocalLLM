f5fb996077d17319e6cd3a9f8d222fb07815a5db
Maarten Smeets <maarten.smeets@eneco.com>
2024-06-27 10:08:53 +0200

    Update

--- scripts/sandboxprep.cmd
+++ scripts/sandboxprep.cmd
@@ -4,5 +4,5 @@ curl -L "https://ollama.com/download/OllamaSetup.exe" --output C:\users\WDAGUtil
 curl -L https://www.python.org/ftp/python/3.12.4/python-3.12.4-amd64.exe --output C:\users\WDAGUtilityAccount\Downloads\python.exe
 C:\users\WDAGUtilityAccount\Downloads\python.exe -Wait /PrependPath=1 /quiet InstallAllUsers=1 TargetDir="C:\python" Include_test=0
 C:\users\WDAGUtilityAccount\Downloads\vscode.exe /verysilent /suppressmsgboxes /MERGETASKS=!runcode
-"C:\Program Files\Microsoft VS Code\Code.exe" C:\users\WDAGUtilityAccount\Documents\Projects\demo
+"C:\Program Files\Microsoft VS Code\Code.exe" C:\users\WDAGUtilityAccount\Documents\Projects\demo --disable-workspace-trust
 
